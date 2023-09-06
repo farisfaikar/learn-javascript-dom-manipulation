c3b2674a6804577a433d9bc69966c07b0b884a38
farisfaikar <farisfaikar.r@gmail.com>
2023-09-06 17:57:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// ========== DOM Manipulation ========== //
+
+// getElementById
+const title = document.getElementById("main-heading");
+console.log(title);
+
+// getElementsByClassName
+const listItem = document.getElementsByClassName("list-item");
+console.log(listItem);
+
+// getElementByTagName
+const listItems2 = document.getElementsByTagName("li");
+console.log(listItems2);
+
+// querySelector
+const container = document.querySelector("div");
+console.log(container);
+
+// querySelectorAll
+const container2 = document.querySelectorAll("div");
+console.log(container2);
